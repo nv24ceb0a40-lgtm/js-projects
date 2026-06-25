a5a8e24cbff833efce5f87045a7bda4f9ed601a7
nv24ceb0a40-lgtm <nv24ceb0a40@student.nitw.ac.in>
2026-06-25 12:00:39 +0530

    done guess-num

---
+++ number-guess/index.js
@@ -0,0 +1,37 @@
+
+const min=document.getElementById("min");
+const max=document.getElementById("max");
+const submitRange=document.getElementById("submitRange");
+const guess=document.getElementById("guess");
+const submitGuess=document.getElementById("submitGuess");
+let guessStatus=document.getElementById("status");
+let secretNum;
+submitRange.addEventListener("click",()=>{
+
+    const minmum=Number(min.value);
+    const maximum=Number(max.value);
+    secretNum=Math.floor( Math.random()*(maximum-minmum+1) + minmum );
+    //console.log(secretNum);
+})
+const resetGame=()=>{
+     min.value = "";
+    max.value = "";
+    guess.value = "";
+}
+submitGuess.addEventListener("click",()=>{
+
+    let guessNum=Number(guess.value);
+    if(guessNum>secretNum)
+    {
+        guessStatus.textContent="lower";
+    }
+    else if(guessNum<secretNum)
+    {
+        guessStatus.textContent="higher"
+    }
+    else
+    {
+        guessStatus.textContent=`you guessed it & game resets`;
+        resetGame();
+    }
+})
